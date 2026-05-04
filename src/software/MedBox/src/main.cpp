22492b123cad01db7edf33a6f057610c16328a7d
marcel <lis.marcys@gmail.com>
2026-05-04 22:17:28 +0200

    [feat] added initial program

--- src/software/MedBox/src/main.cpp
+++ src/software/MedBox/src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Definiujemy piny dla naszych diod testowych
+#define LED1_PIN 12
+#define LED2_PIN 13
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Uruchamiamy port szeregowy, żeby widzieć komunikaty w terminalu PlatformIO
+  Serial.begin(115200);
+  Serial.println("\nStart testu diod na ESP8266!");
+
+  // Konfigurujemy piny jako wyjścia
+  pinMode(LED1_PIN, OUTPUT);
+  pinMode(LED2_PIN, OUTPUT);
+
+  // Ustawiamy stan początkowy (LED1 włączony, LED2 wyłączony)
+  digitalWrite(LED1_PIN, HIGH);
+  digitalWrite(LED2_PIN, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Przełączamy stany diod na przeciwne
+  digitalWrite(LED1_PIN, !digitalRead(LED1_PIN));
+  digitalWrite(LED2_PIN, !digitalRead(LED2_PIN));
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Wypisujemy kontrolnie kropkę w terminalu
+  Serial.print(".");
+
+  // Czekamy 500 milisekund (pół sekundy)
+  delay(500);
 }
